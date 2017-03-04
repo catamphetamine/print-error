ab5a3d29b2dfc97dfbaf7901feaa265ed6c789fb
Nikolay <halt.hammerzeit.at@gmail.com>
2017-03-04 14:57:47 +0300

    Dropping pretty-error for now

--- source/terminal.js
+++ source/terminal.js
@@ -3,5 +3,10 @@ const printer = new pretty_error()
 
 export default function render(error)
 {
-	return printer.render(error)
+	// Until the bugs are fixed in `pretty-error`
+	// https://github.com/AriaMinaei/pretty-error/issues/40
+	// https://github.com/AriaMinaei/RenderKid/issues/7
+	// return printer.render(error)
+
+	return error.stack
 }
